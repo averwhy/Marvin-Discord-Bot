bc29f6547ca3b8c54183b70ace3e7c26a8796376
averyb <43783082+avrwhy@users.noreply.github.com>
2019-04-09 08:13:22 -0400

    it works now

--- Marvin-Main.py
+++ Marvin-Main.py
@@ -3,74 +3,16 @@ import aiohttp
 import time
 from discord.ext import commands
 
-TOKEN = 'token here'
-userid = 'id here'
+TOKEN = ''
+userid = ''
 version = 'v1.1'
 client = discord.Client()
 ts = time.gmtime()
 
-#this \/ is the command code
-quotes = [
-    "Sorry, did I say something wrong? Pardon me for breathing which I never do anyway so I don't know why I bother to say it.",
-    "I would correct your grammar as well, but you wouldn't listen. No one ever does...",
-    "I'd make a suggestion, but you wouldn't listen. No one ever does."
-    ]
-
-@client.event
-async def on_message(message):
-    # We don't want the bot to reply to itself
-    if message.author == client.user:
-        return
-    #or other bots
-    if message.author.bot:
-        return
-    if message.content.startswith('m|bot'):
-        msg = "Hi, I'm Marvin. I'm a bot that originated on r/scp and other SCP subreddits. I reply to people when i detect an SCP classification, such as SCP-280. I may not always be right."
-        await client.send_message(message.channel, msg)
-        print("[",time.strftime("%Y-%m-%d %H:%M:%S",ts),"]Someone used about.")
-        
-    if message.content.startswith('m|quote'):
-        msg = "I wish i could tell you a quote, but my developer is so bad at what he does i can't."
-        await client.send_message(message.channel, msg)
-        print("[",time.strftime("%Y-%m-%d %H:%M:%S",ts),"]Someone used quote.")
-
-    if message.content.startswith('m|getinvite'):
-        msg = '{0.author.mention}, the invite code is https://discord.gg/WQJ7hRF'.format(message)
-        await client.send_message(message.channel, msg)
-        print("[",time.strftime("%Y-%m-%d %H:%M:%S",ts),"]Someone used getinvite.")
-        
-    if message.content.startswith('m|howto'):
-        embed = discord.Embed(title="**How to use Marvin**", description="How Marvin will find any SCP for you", colour=discord.Colour(0x7a19fd))
-        embed.set_author(name="Marvin")
-        embed.set_footer(text="Marvin v1.1")
-        embed.add_field(name="Main Use", value="For Marvin to find an SCP for you, say 'SCP-XXX' and replace the x's with any number 001-4999.", inline=False)
-        embed.add_field(name="What doesn't work", value="Marvin can find you a lot of SCP's.. Except -J's. Also, be sure to use '001' or '037', not '1' or '37'.", inline=False)
-        await client.send_message(message.channel, embed=embed)
-        print("[",time.strftime("%Y-%m-%d %H:%M:%S",ts),"]Someone used howto.")
-
-    if message.content.startswith('m|scp'):
-        msg = 'The SCP Foundation is a secret organization entrusted by governments around the globe to contain and study anomalous individuals, entities, locations, objects, and phenomena operating outside the bounds of natural law. _Note: It is all entirely fictional. Any events that occur are purely coincidental._'
-        await client.send_message(message.channel, msg)
-        print("[",time.strftime("%Y-%m-%d %H:%M:%S",ts),"]Someone used about scp.")
-    
-    if message.content.startswith('m|cmds'):
-            embed = discord.Embed(title="**List of commands**", description="List of commands to use.", colour=discord.Colour(0x7a19fd))
-            embed.set_author(name="Marvin")
-            embed.set_footer(text="Marvin v1.1")
-            embed.add_field(name="m|bot", value="Bot info", inline=False)
-            embed.add_field(name="m|scp", value="Info about the SCP Foundation", inline=False)
-            embed.add_field(name="m|howto", value="How to use the main feature of this bot", inline=False)
-            embed.add_field(name="m|getinvite", value="Invite to the bot support server", inline=False)
-            embed.add_field(name="m|quote", value="Recite a quote.", inline=False)
-            await client.send_message(message.channel, embed=embed)
-            print("[",time.strftime("%Y-%m-%d %H:%M:%S",ts),"]Someone viewed the list of commands.")
-            
-            
-#The code in between these \/ hashtag lines are the classification detection code
 ###########################################################################################################################
 #BIG THANKS TO ALBERTO POLIJAK FOR THE FOLLOWING CODE
-    # Number separator is character between SCP and a number, example with hyphen:
-    # SCP-1
+    # Number separator is character between SCP and a number, example with space:
+    # SCP 1
 NUMBER_SEPARATOR = "-"
 MAXIMUM_SCP_NUMBER = 4999
 def get_scp_link(message_content):
@@ -124,15 +66,72 @@ def _build_scp_url(formatted_scp_number):
     base_url = "http://www.scp-wiki.net/scp-"
     prefix = "SCP-" + formatted_scp_number + ": "
     return prefix + base_url + formatted_scp_number
+    
+#https://scp-wiki.net/scp-001
+###########################################################################################################################
+#and then the rest of this code is mine
+
+quotes = [
+    "Sorry, did I say something wrong? Pardon me for breathing which I never do anyway so I don't know why I bother to say it.",
+    "I would correct your grammar as well, but you wouldn't listen. No one ever does...",
+    "I'd make a suggestion, but you wouldn't listen. No one ever does."
+    ]
+
 @client.event
 async def on_message(message):
+    # We don't want the bot to reply to itself
+    if message.author == client.user:
+        return
+    #or other bots
+    if message.author.bot:
+        return
+    if message.content.startswith('m|bot'):
+        msg = "Hi, I'm Marvin. I'm a bot that originated on r/scp and other SCP subreddits. I reply to people when i detect an SCP classification, such as SCP-280. I may not always be right."
+        await client.send_message(message.channel, msg)
+        print("[",time.strftime("%Y-%m-%d %H:%M:%S",ts),"]Someone used about.")
+        
+    if message.content.startswith('m|quote'):
+        msg = "I wish i could tell you a quote, but my developer is so bad at what he does i can't."
+        await client.send_message(message.channel, msg)
+        print("[",time.strftime("%Y-%m-%d %H:%M:%S",ts),"]Someone used quote.")
+
+    if message.content.startswith('m|getinvite'):
+        msg = '{0.author.mention}, the invite code is https://discord.gg/WQJ7hRF'.format(message)
+        await client.send_message(message.channel, msg)
+        print("[",time.strftime("%Y-%m-%d %H:%M:%S",ts),"]Someone used getinvite.")
+        
+    if message.content.startswith('m|howto'):
+        embed = discord.Embed(title="**How to use Marvin**", description="How Marvin will find any SCP for you", colour=discord.Colour(0x7a19fd))
+        embed.set_author(name="Marvin")
+        embed.set_footer(text="Marvin v1.1")
+        embed.add_field(name="Main Use", value="For Marvin to find an SCP for you, say 'SCP-XXX' and replace the x's with any number 001-4999. Even saying SCP-1 works.", inline=False)
+        embed.add_field(name="What doesn't work", value="Marvin can find you a lot of SCP's.. Except -J's. It will also not work if you put other text in there, so just say SCP-XXX.", inline=False)
+        embed.add_field(name="Who made it work", value="Alberto Polijak on Stack Overflow came up with the classification code, so big thanks to him.", inline=False)
+        await client.send_message(message.channel, embed=embed)
+        print("[",time.strftime("%Y-%m-%d %H:%M:%S",ts),"]Someone used howto.")
+
+    if message.content.startswith('m|scp'):
+        msg = 'The SCP Foundation is a secret organization entrusted by governments around the globe to contain and study anomalous individuals, entities, locations, objects, and phenomena operating outside the bounds of natural law. _Note: It is all entirely fictional. Any events that occur are purely coincidental._'
+        await client.send_message(message.channel, msg)
+        print("[",time.strftime("%Y-%m-%d %H:%M:%S",ts),"]Someone used about scp.")
+    
+    if message.content.startswith('m|cmds'):
+            embed = discord.Embed(title="**List of commands**", description="List of commands to use.", colour=discord.Colour(0x7a19fd))
+            embed.set_author(name="Marvin")
+            embed.set_footer(text="Marvin v1.1")
+            embed.add_field(name="m|bot", value="Bot info", inline=False)
+            embed.add_field(name="m|scp", value="Info about the SCP Foundation", inline=False)
+            embed.add_field(name="m|howto", value="How to use the main feature of this bot", inline=False)
+            embed.add_field(name="m|getinvite", value="Invite to the bot support server", inline=False)
+            embed.add_field(name="m|quote", value="Recite a quote.", inline=False)
+            await client.send_message(message.channel, embed=embed)
+            print("[",time.strftime("%Y-%m-%d %H:%M:%S",ts),"]Someone viewed the list of commands.")
+
     if "SCP" in message.content:
         scp_link = get_scp_link(message.content)
         if scp_link is not None:
             await client.send_message(message.channel, scp_link)
-#https://scp-wiki.net/scp-001
-###########################################################################################################################
-#and then the rest of this code is mine
+
 @client.event
 async def on_ready():
     print('------------------------------------------------')
